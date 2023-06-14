c5f4e6667193265605ae1bcb9dbb3f852b3ac0ef
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-06-14 20:24:46 +0200

    kma trails 28

--- conda/build.sh
+++ conda/build.sh
@@ -5,9 +5,3 @@ mkdir -p ${PREFIX}/bin
 cp $BINARIES $PREFIX/bin
 mkdir -p $PREFIX/doc/kma
 cp README.md $PREFIX/doc/kma/
-
-export CXXFLAGS_DEFAULT=${CXXFLAGS}
-    export CXXFLAGS="${CXXFLAGS} -isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
-    export INSTALL_NAME_TOOL=${INSTALL_NAME_TOOL:-install_name_tool}
-fi
-scons -Q PREFIX=${PREFIX} install
