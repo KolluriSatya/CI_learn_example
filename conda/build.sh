25313e22b29f28b2ece616cf919f420a2604c4dd
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-06-14 16:14:47 +0200

    kma trails 17

--- conda/build.sh
+++ conda/build.sh
@@ -2,7 +2,7 @@ BINARIES="kma kma_index kma_shm kma_update"
 make CFLAGS="-w -O3 -I$PREFIX/include -L$PREFIX/lib"
 
 mkdir -p ${PREFIX}/bin
-cp $BINARIES $PREFIX/bin "${PREFIX}"/bin/systemd-socket-activate_"${python=3.10}"
+cp $BINARIES $PREFIX/bin
 mkdir -p $PREFIX/doc/kma
 
 cp README.md $PREFIX/doc/kma/
