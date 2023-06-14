bac70150029f80c2e6fad1a8bda0fe5b86a8ef5c
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-06-14 17:16:36 +0200

    kma trails 21

--- conda/build.sh
+++ conda/build.sh
@@ -1,8 +1,8 @@
 BINARIES="kma kma_index kma_shm kma_update"
 make CFLAGS="-w -O3 -I$PREFIX/include -L$PREFIX/lib"
 
-mkdir -p ${PREFIX}/bin
-cp $BINARIES $PREFIX/bin
+mkdir -p ${PREFIX}/bin/gcc
+cp $BINARIES $PREFIX/bin/gcc
 mkdir -p $PREFIX/doc/kma
 
 cp README.md $PREFIX/doc/kma/
