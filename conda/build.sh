76b1b3532e27c0148bc480f8a257a5d714ed4c32
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-06-15 10:30:38 +0200

    kma trail 42

--- conda/build.sh
+++ conda/build.sh
@@ -3,5 +3,13 @@ make CFLAGS="-w -O3 -I$PREFIX/include -L$PREFIX/lib"
 
 mkdir -p ${PREFIX}/bin
 cp $BINARIES $PREFIX/bin
+
+CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX}/bin \
+  ${CMAKE_PLATFORM_FLAGS[@]} \
+  ${PREFIX}/bin
+
+
 mkdir -p $PREFIX/doc/kma
 cp README.md $PREFIX/doc/kma/
