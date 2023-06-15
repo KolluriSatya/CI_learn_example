0a97cd3bf6fa29306dfe936dd623c1dce666ff84
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-06-15 11:05:37 +0200

    kma trail 45

--- conda/build.sh
+++ conda/build.sh
@@ -1,15 +1,9 @@
 BINARIES="kma kma_index kma_shm kma_update"
 make CFLAGS="-w -O3 -I$PREFIX/include -L$PREFIX/lib"
-
 mkdir -p ${PREFIX}/bin
 cp $BINARIES $PREFIX/bin
 
-CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX}/bin \
-  ${CMAKE_PLATFORM_FLAGS[@]} \
-  ${PREFIX}/bin
-
+export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
 
 mkdir -p $PREFIX/doc/kma
 cp README.md $PREFIX/doc/kma/
