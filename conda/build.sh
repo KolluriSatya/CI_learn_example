da33ed93c3b9ce3c0aff30d8afb74e9f86a31484
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-06-16 09:25:05 +0200

    kma trail 60

--- conda/build.sh
+++ conda/build.sh
@@ -1,5 +1,3 @@
-export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-export CONDA_BUILD_SYSROOT= $PREFIX/bin
 BINARIES="kma kma_index kma_shm kma_update"
 cp CFLAGS="-w -O3 -I$PREFIX/include -L$PREFIX/lib"
 mkdir -p ${PREFIX}/bin
