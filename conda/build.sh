a106ad61099235f7daf9fe4b70e0b99cbe2faab4
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-06-18 13:49:09 +0200

    kma trail 67

--- conda/build.sh
+++ conda/build.sh
@@ -2,7 +2,6 @@ BINARIES="kma kma_index kma_shm kma_update"
 cp CFLAGS="-w -O3 -I$PREFIX/include -L$PREFIX/lib"
 mkdir -p ${PREFIX}/bin
 cp $BINARIES $PREFIX/bin
-
 mkdir -p $PREFIX/doc/kma
 cp README.md $PREFIX/doc/kma/
-
+"{{ PYTHON }} -m pip install . --no-deps --ignore-installed -vvv"
