54cc28653435f3f73cfeef0f1836c586b4ce551a
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-10-04 10:39:11 +0200

    check resfinder

--- conda/build.sh
+++ conda/build.sh
@@ -3,7 +3,7 @@
 mkdir -p ${PREFIX}/bin
 
 chmod +x src/resfinder/cge/resfinder.py
-cp resfinder.py ${PREFIX}/bin/resfinder.py
+cp src/resfinder/cge/resfinder.py ${PREFIX}/bin/resfinder.py
 
 # copy script to download database
 chmod +x ${RECIPE_DIR}/download-resfinder-db.sh
