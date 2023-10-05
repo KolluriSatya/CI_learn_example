ba622b31406397b8974a735e04cfc6939afb3a11
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-10-05 08:43:17 +0200

    chk resfinder dpdncs

--- conda/make_conda_linux.py
+++ conda/make_conda_linux.py
@@ -17,9 +17,8 @@ build['number'] = 0
 build['noarch'] = 'python'
 
 requirements = yaml.comments.CommentedMap()
-
 requirements['host'] = ['python >=3.8', 'kma', 'wget']
-requirements['run'] = ['cgecore >=1.5.6', 'tabulate >=0.8.9', 'pandas >=1.4.2', 'biopython >=1.79']
+requirements['run'] = ['tabulate >=0.8.9', 'pandas >=1.4.2', 'biopython >=1.79']
 
 about = yaml.comments.CommentedMap()
 about['home'] = 'https://bitbucket.org/genomicepidemiology/resfinder'
