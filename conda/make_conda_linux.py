231331485c22d1fcc406c1a20f749f1a1a634d0d
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-10-09 09:41:55 +0200

    chk dependencies

--- conda/make_conda_linux.py
+++ conda/make_conda_linux.py
@@ -1,51 +1,59 @@
-import yaml
-import os
-import sys
+import ruamel.yaml as yaml
 
-sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)), '')] + sys.path
+# Create an ordered dictionary for each section
+package = yaml.comments.CommentedMap()
+package['name'] = 'resfinder'
 
-data = {
-    "package": {
-        "name": "resfinder",
-        "version": "4.3.3"
-},
-    "source": {
-        "url": "https://bitbucket.org/genomicepidemiology/{}/get/{}.tar.gz".format(package['name'], package['version'])
-    },
-    "build": {
-        "number": 0,
-        "noarch": "python",
-        "script": "{{ PYTHON }} -m pip install . --no-deps --ignore-installed -vvv"
-    },
-    "requirements": {
-        "host": [
-            "python==3.8",
-            "pip"
-            "cgelib>=0.7.3",
-            "cgecore==1.5.6",
-            "tabulate>=0.8.9",
-            "pandas>=1.4.2",
-            "biopython>=1.79"
-        ],
-        "run": [
-            "cgelib>=0.7.3",
-            "cgecore==1.5.6",
-            "tabulate>=0.8.9",
-            "pandas>=1.4.2",
-            "biopython>=1.79"
-        ]
-    },
-    "about": {
-        "home": "https://github.com/genomicepidemiology/resfinder",
-        "summary": "resfinder test.",
-        "license": "Apache-2.0"
-    }
-}
+with open('src/resfinder/version.py', 'r') as f:
+    for line in f:
+        if line.startswith('#define'):
+            package['version'] = line.split()[2].replace("\"", "")
 
-# Convert the data to YAML and print it
+source = yaml.comments.CommentedMap()
+source['url'] = 'https://bitbucket.org/genomicepidemiology/{}/get/{}.tar.gz'.format(package['name'], package['version'])
+
+build = yaml.comments.CommentedMap()
+build['number'] = 0
+build['noarch'] = 'python'
+
+requirements = yaml.comments.CommentedMap()
+requirements['build'] = [ "cgelib>=0.7.3",
+                          "cgecore==1.5.6",
+                          "tabulate>=0.8.9",
+                          "pandas>=1.4.2",
+                          "biopython>=1.79"]
+requirements['host'] = [ "cgelib>=0.7.3",
+                         "cgecore==1.5.6",
+                         "tabulate>=0.8.9",
+                         "pandas>=1.4.2",
+                         "biopython>=1.79"]
+requirements['run'] = [ "cgelib>=0.7.3",
+                        "cgecore==1.5.6",
+                        "tabulate>=0.8.9",
+                        "pandas>=1.4.2",
+                        "biopython>=1.79"]
+
+about = yaml.comments.CommentedMap()
+about['home'] = 'https://bitbucket.org/genomicepidemiology/resfinder'
+about['summary'] = 'resfinder test'
+about['license'] = 'Apache-2.0'
+
+extra = yaml.comments.CommentedMap()
+identifiers = yaml.comments.CommentedMap()
+extra['identifiers'] = identifiers
+
+# Create a dictionary for the entire YAML content
+data = yaml.comments.CommentedMap()
+data['package'] = package
+data['source'] = source
+data['build'] = build
+data['requirements'] = requirements
+data['about'] = about
+data['extra'] = extra
+
+# Serialize the data to YAML and print it
 yaml_str = yaml.dump(data, Dumper=yaml.RoundTripDumper).replace("\"{{", "{{").replace("}}\"", "}}")
 print(yaml_str)
 
 with open('conda/meta.yaml', 'w') as f:
-    f.write(yaml_str)
     f.write(yaml_str)
