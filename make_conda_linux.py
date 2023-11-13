bda76aac78c602344028a003ffdcdfccb7fbd98f
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-11-13 09:50:39 +0100

    chk resfinder

--- make_conda_linux.py
+++ make_conda_linux.py
@@ -25,7 +25,7 @@ data = {
         "host": [
             "python>=3.8",
             "kma>=1.4.9",
-            "cgecore>=1.5.6",
+            "cgecore",
             "tabulate>=0.8.9",
             "pandas>=1.4.2",
             "biopython>=1.79"
@@ -33,7 +33,7 @@ data = {
         "run": [
             "python>=3.8",
             "kma>=1.4.9",
-            "cgecore>=1.5.6",
+            "cgecore",
             "tabulate>=0.8.9",
             "pandas>=1.4.2",
             "biopython>=1.79"
